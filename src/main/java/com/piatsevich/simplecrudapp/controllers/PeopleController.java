1c947e51fbc6f381e80cb7740b92a231e5253d15
Andrei Piatsevich <apiats2@gmail.com>
2023-02-19 22:17:00 +0300

    add models

---
+++ src/main/java/com/piatsevich/simplecrudapp/controllers/PeopleController.java
@@ -0,0 +1,72 @@
+package com.piatsevich.simplecrudapp.controllers;
+
+import com.piatsevich.simplecrudapp.dao.PersonDao;
+import com.piatsevich.simplecrudapp.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDao personDao;
+
+    @Autowired
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDao.index());
+        return "people/index";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+
+//    @GetMapping("/new")
+//    public String newPerson(Model model) {
+//        model.addAttribute("person", new Person());
+//        return "people/new";
+//    }
+
+    // alternative//
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute Person person) {
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDao.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "redirect:/people";
+    }
+
+
+
+
+}
